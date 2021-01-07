edca0275af4ca64992fce2ec2f6419d45683ab6b
hbmin3789 <hbmin3789@naver.com>
2021-01-07 10:47:43 +0900

    chore : add viewmodel project

---
+++ MapleUtils.Core.ViewModels/Common/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapleUtils.Core.ViewModels.Common
+{
+    public abstract class ViewModelBase : BindableBase
+    {
+        protected abstract void InitVariables();
+        protected abstract void InitCommands();
+    }
+}
---
+++ MapleUtils.Core.ViewModels/SearchViewModel.cs
@@ -0,0 +1,49 @@
+﻿using MapleUtils.Core.ViewModels.Common;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapleUtils.Core.ViewModels
+{
+    public class SearchViewModel : ViewModelBase
+    {
+        #region Properties
+
+        private string _keyWord;
+        public string KeyWord
+        {
+            get => _keyWord;
+            set => SetProperty(ref _keyWord, value);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public DelegateCommand SearchCommand { get; set; }
+
+        #endregion
+
+        public SearchViewModel()
+        {
+
+        }
+
+        #region Initialize
+
+        protected override void InitVariables()
+        {
+
+        }
+
+        protected override void InitCommands()
+        {
+            
+        }
+
+        #endregion
+
+    }
+}
