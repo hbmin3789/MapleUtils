aca51063d1f903db6aabc5869e59be9a85a80fe7
hbmin3789 <hbmin3789@naver.com>
2021-01-10 13:24:19 +0900

    feat : add SkillViewModel add SkillViewModel

---
+++ MapleUtils.Core.ViewModels/SkillViewModel.cs
@@ -0,0 +1,65 @@
+﻿using MapleStoryHelper.Standard.SkillLib.Model;
+using MapleUtils.Core.ViewModels.Common;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MapleUtils.Core.ViewModels
+{
+    public class SkillViewModel : ViewModelBase
+    {
+        private ObservableCollection<SkillBase> _keyboardLayer;
+        public ObservableCollection<SkillBase> KeyboardLayer
+        {
+            get => _keyboardLayer;
+            set => SetProperty(ref _keyboardLayer, value);
+        }            
+
+        private ObservableCollection<SkillBase> _skillItems;
+        public ObservableCollection<SkillBase> SkillItems
+        {
+            get => _skillItems;
+            set => SetProperty(ref _skillItems, value);
+        }
+
+        private SkillBase _selectedKey;
+        public SkillBase SelectedKey
+        {
+            get => _selectedKey;
+            set => SetProperty(ref _selectedKey, value);
+        }
+
+        public DelegateCommand SetSkillCommand { get; set; }
+
+        public SkillViewModel()
+        {
+            InitVariables();
+            InitCommands();
+        }
+
+
+        protected override void InitVariables()
+        {
+            _skillItems = new ObservableCollection<SkillBase>();
+            _keyboardLayer = new ObservableCollection<SkillBase>();
+        }
+
+        protected override void InitCommands()
+        {
+            SetSkillCommand = new DelegateCommand(SetSkill);
+        }
+
+        private void SetSkill()
+        {
+            var idx = KeyboardLayer.IndexOf(SelectedKey);
+        }
+
+        protected override void OnNavigate(Type t)
+        {
+            
+        }
+    }
+}
